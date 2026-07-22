be5e66613ffedbb0b4299fe090708a01cea1481a
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-22 16:37:14 -0300

    feat: adiciona model referente aos contatos do usuario

--- app/users/infrastructure/models.py
+++ app/users/infrastructure/models.py
@@ -16,11 +16,28 @@ class User(models.Model):
     session = models.CharField(max_length=100)
     session_started = models.BooleanField(default=False)
 
+    access_contacts = models.BooleanField(default=False)
+
     created_at = models.DateTimeField(auto_now_add=True)
     deleted_at = models.DateTimeField(null=True)
 
     class Meta:
         db_table = 'users'
+
+
+class Contact(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False)
+    name = models.CharField(max_length=130)
+    number = models.CharField(max_length=180)
+    lid = models.CharField(max_length=180)
+    user = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'contacts'
 
 
 class RefreshToken(models.Model):
