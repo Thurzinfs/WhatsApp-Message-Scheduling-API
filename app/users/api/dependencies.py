4ce7d00fef8a9258e9397ded6eab6bf36ec9cfbf
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-23 00:20:23 -0300

    feat: adiciona injecao de dependencia das funcionalidades referentes a sincronizacao de contatos

--- app/users/api/dependencies.py
+++ app/users/api/dependencies.py
@@ -2,18 +2,23 @@ from dependency_injector import containers, providers
 
 from app.users.application.use_cases import (
     DeactiveUserUseCase,
+    EnableSyncContactsUseCase,
+    ListContactsByUserUseCase,
     LoginUseCase,
     LoginWahaForWhatsAppQrCodeUseCase,
     RegisterUserUseCase,
     RequestCodeLoginWhatsAppUseCase,
     ResponseUserByEmailUseCase,
     ResponseUserByIDUseCase,
+    SyncContactsUserUseCase,
 )
+from app.users.infrastructure.adapters import ContactsSyncTaskAdapter
 from app.users.infrastructure.repository import (
+    ContactRepository,
     RefreshTokenRepository,
     UserRepository,
 )
-from app.users.infrastructure.services import HashService, TokenService
+from app.users.infrastructure.services import FilterContactsService, HashService, TokenService
 
 
 class UserContainer(containers.DeclarativeContainer):
@@ -23,9 +28,15 @@ class UserContainer(containers.DeclarativeContainer):
 
     token_repo = providers.Factory(RefreshTokenRepository)
 
+    contact_repo = providers.Factory(ContactRepository)
+
     token_service = providers.Factory(TokenService)
 
     hash_service = providers.Factory(HashService)
+
+    filter_service = providers.Factory(FilterContactsService)
+
+    task_sync_adapter = providers.Factory(ContactsSyncTaskAdapter)
 
     register_user_use_case = providers.Factory(
         RegisterUserUseCase,
@@ -48,6 +59,20 @@ class UserContainer(containers.DeclarativeContainer):
         waha_adapter=core.waha_adapter,
     )
 
+    enable_sync_contacts_use_case = providers.Factory(
+        EnableSyncContactsUseCase,
+        user_repo=user_repo,
+        task_sync_adapter=task_sync_adapter
+    )
+
+    sync_contacts_user_use_case = providers.Factory(
+        SyncContactsUserUseCase,
+        user_repo=user_repo,
+        waha_adapter=core.waha_adapter,
+        filter_service=filter_service,
+        contact_repo=contact_repo
+    )
+
     login_use_case = providers.Factory(
         LoginUseCase,
         user_repo=user_repo,
@@ -66,4 +91,9 @@ class UserContainer(containers.DeclarativeContainer):
         RequestCodeLoginWhatsAppUseCase,
         user_repo=user_repo,
         waha_adapter=core.waha_adapter,
+    )
+
+    list_contacts_by_user_use_case = providers.Factory(
+        ListContactsByUserUseCase,
+        contact_repo=contact_repo
     )
