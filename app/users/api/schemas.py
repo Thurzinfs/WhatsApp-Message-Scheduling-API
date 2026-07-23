c298545f17ed882ca91a2d5c1389c195d2286f3a
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-23 00:16:02 -0300

    feat: adiciona campo de configuracao de usuario e corrige campos presentes em contacts

--- app/users/api/schemas.py
+++ app/users/api/schemas.py
@@ -8,7 +8,7 @@ from pydantic import EmailStr
 from app.users.application.dto import (
     ContactInDTO,
     ContactUpdateInDTO,
-    CotnactOutDTO,
+    ContactOutDTO,
     LoginInDTO,
     LoginOutDTO,
     QrCodeOutDTO,
@@ -40,6 +40,7 @@ class UserOutSchema(Schema):
     connected: bool
     session: str
     session_started: bool
+    access_contacts: bool
     created_at: datetime
     deleted_at: datetime | None = None
 
@@ -53,6 +54,7 @@ class UserOutSchema(Schema):
             connected=user_dto.connected,
             session=user_dto.session,
             session_started=user_dto.session_started,
+            access_contacts=user_dto.access_contacts,
             created_at=user_dto.created_at,
             deleted_at=user_dto.deleted_at,
         )
@@ -61,32 +63,30 @@ class UserOutSchema(Schema):
 class ContactInSchema(Schema):
     name: str
     number: str
-    lid: str
     user: UUID
 
     def to_dto(self) -> ContactInDTO:
         return ContactInDTO(
             name=self.name,
             number=self.number,
-            lid=self.lid,
             user=self.user
         )
 
 
 class ContactOutSchema(Schema):
     id: UUID
+    contact_id: str
     name: str
     number: str
-    lid: str
     user: UUID
 
     @staticmethod
-    def from_domain(dto: CotnactOutDTO):
+    def from_domain(dto: ContactOutDTO):
         return ContactOutSchema(
             id=dto.id,
+            contact_id=dto.contact_id,
             name=dto.name,
             number=dto.number,
-            lid=dto.lid,
             user=dto.user
         )
 
