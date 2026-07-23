484e7475c9f6098f9a1c2c35cd2e4f0302d90ea4
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-23 00:21:18 -0300

    feat: implementa a parte de contatos a aplicacao

--- config/api.py
+++ config/api.py
@@ -2,7 +2,7 @@ from ninja import NinjaAPI
 
 from app.message.api.views import router as router_messages
 
-from app.users.api.views import router as router_users, auth_router
+from app.users.api.views import router as router_users, auth_router, contact_router
 
 
 api = NinjaAPI(title='Scheduled Messages for WhatsApp', docs_url='/docs/')
@@ -10,3 +10,4 @@ api = NinjaAPI(title='Scheduled Messages for WhatsApp', docs_url='/docs/')
 api.add_router('/auth', auth_router, tags=['Auth'])
 api.add_router('/users', router_users, tags=['User'])
 api.add_router('/message', router_messages, tags=['Message'])
+api.add_router('/contacts', contact_router, tags=['Contact'])
