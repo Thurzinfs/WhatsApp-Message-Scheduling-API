9b4c1045ef4547f05748125bae72cc3b9376f187
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-24 11:35:49 -0300

    fix: correcao do value object referente ao numero de contato

--- app/users/domain/value_objects.py
+++ app/users/domain/value_objects.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass
 
+from core.exceptions import BaseDomainException
+
 
 @dataclass
 class PhoneNumberVO:
     value: str
 
     def __post_init__(self):
-        if (len(self.value) > 13 and len(self.value) < 12) or '@c.us' not in self.value:
-            self.value = f'{self.value}@c.us'
+        if len(self.value) > 13 or len(self.value) < 12:
+            raise BaseDomainException('phone number is invalid')
+    
