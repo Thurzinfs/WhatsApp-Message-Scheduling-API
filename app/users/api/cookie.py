d28eb549bdb59680b86bbc08d3c11b7e4c7dffc6
Thurzinfs <francasilvaarthur95@gmail.com>
2026-07-29 10:30:46 -0300

    feat: implementa a nova autenticacao por uso de cookies

--- app/users/api/cookie.py
+++ app/users/api/cookie.py
@@ -2,26 +2,28 @@ from typing import Any
 
 from django.http import HttpRequest
 import jwt
-from ninja.security import HttpBearer
+from ninja.security import APIKeyCookie
 
 from app.users.infrastructure.models import User
 from config import settings
 from core.exceptions import BaseDomainException
 
+class AuthCookie(APIKeyCookie):
+    param_name = 'auth_token'
 
-class AuthBearer(HttpBearer):
-    def authenticate(self, request: HttpRequest, token: str) -> Any | None:
+    def authenticate(self, request: HttpRequest, key: str) -> Any | None:
         try:
             payload = jwt.decode(
-                token,
-                settings.JWT_SECRET_KEY,
-                algorithms=[settings.JWT_ALGORITHIM],
+                key, 
+                settings.JWT_SECRET_KEY, 
+                algorithms=[settings.JWT_ALGORITHIM]
             )
+
             user = User.objects.get(id=payload['sub'])
             if not user:
                 raise BaseDomainException('user not found')
-
+            
             return user
 
-        except jwt.PyJWTError:
+        except (jwt.PyJWTError, User.DoesNotExist):
             return None
